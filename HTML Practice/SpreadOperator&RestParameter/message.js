935223fa43611769b1625ca4aa378e09ffa56148
unknown <himanshujain.adt@gmail.com>
2024-09-25 18:03:31 +0530

    done

---
+++ HTML Practice/SpreadOperator&RestParameter/message.js
@@ -0,0 +1,7 @@
+const message = () => {
+    const name = "Himanshu Jain";
+    const address = "Indore";
+    const age = "32"
+    return name + " is" + age + " years old";
+};
+export default message;
---
+++ HTML Practice/SpreadOperator&RestParameter/messagedata.js
@@ -0,0 +1,6 @@
+const message=()=>{
+    const id=123;
+    const address="Indore";
+    return "id is:"+id+"  address : "+address;
+};
+export default message;
